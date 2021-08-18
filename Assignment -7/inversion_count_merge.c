ff15714900d9f0a40707f9dedbfee7732414c923
Jashu Bhardwaj <74259821+JashuBhardwaj@users.noreply.github.com>
2021-08-18 19:25:26 +0530

    Update inversion_count_merge.c

--- Assignment -7/inversion_count_merge.c
+++ Assignment -7/inversion_count_merge.c
@@ -1,96 +1,66 @@
 #include <stdio.h>
 
-int merge_sort(int arr[], int s, int end) {
-    if(s >= end) {
-        return 0;
+void merge(int arr[], int s, int mid, int end) {
+    
+    int i = s;
+    int j = mid + 1;
+    int n = end - s + 1;
+    int left[n];
+    
+    int k = 0;
+    
+    while(i <= mid && j <= end) {
+        if(arr[i] > arr[j]) {
+            arr2[k++] = arr[j++];
+        }
+        else {
+            arr2[k++] = arr[i++];
+        }
     }
     
-    int mid = (s + end) / 2;
+    while(i <= mid) {
+        arr2[k++] = arr[i++];
+    }
     
+    while(j <= mid) {
+        arr2[k++] = arr[j++];
+    }
+    
+    for(i = 0; i < n; i++){
+        arr[s + i] = arr2[i];
+    }
+    
+}
+
+void merge_sort(int arr[], int s, int end) {
+    
+    if(s >= end) {
+        return;
+    }
+    
+    int mid = (s + mid)/2;
     merge_sort(arr, s, mid);
     merge_sort(arr, mid + 1, end);
     
-    int c = 0;
-    
-    c += merge(arr, s, mid, end);
-    
-    return c;
+    merge(arr, s, mid, end);
 }
 
-int merge(int arr[], int s, int mid, int end) {
-    
-    int i = 0;
-    int j = 0;
-    int c = 0;
-    
-    int arr2[end + 1];
-    int k = end;
-    i = s;
-    
-    j = mid + 1;
-    
-    while(i >= mid || j >= end) {
-        if(arr[i] > arr[j]) {
-            c++;
-            arr2[k] = arr[i];
-            i++;
-            k--;
-            
-        }
-        
-        else {
-            arr2[k] = arr[j];
-            j++;
-            k--;
-        }
-    }
-    
-    while(i >= mid) {
-        arr2[k] = arr[i];
-        i++;
-        k--;
-    }
-    
-    while(j >= end) {
-        arr2[k] = arr[j];
-        j++;
-        k--;
-    }
-    
-    for(i = s; i <= end; i++) {
-        arr[i] = arr2[j];
-        
-    }
-    
-    return c;
-    
-    
-}
+
+
 
 int main()
 {
-    int n = 0;
-    
-    printf("Enter Array size : ");
-    scanf("%d", &n);
-    
-    int arr[n];
-    
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
+    int arr[4] = {8,4,2,1};
     
     int s = 0;
+    int end = 4 - 1;
     
-    int end = n - 1;
+    merge_sort(arr, s, end);
     
-    int count = merge_sort(arr, s, end);
-    
-    printf("%d", count);
-    
-    for(int i = 0; i < n; i++) {
-        printf("%d", arr[i]);
+    for(int i = 0; i < 4; i++){
+        printf("%d ", arr[i]);
     }
+
 
     return 0;
 }
